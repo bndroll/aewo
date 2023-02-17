a7a119c10e94ba4f4cda258e3aeeda5764acbe57
Jho00 <akutin_artem@mail.ru>
2023-02-18 03:12:19 +0400

    mapper updated

--- backend/src/entity/ExcelMappingEntity.ts
+++ backend/src/entity/ExcelMappingEntity.ts
@@ -17,7 +17,43 @@ export class ExcelMappingEntity {
 
     @Column({
         nullable: false,
+        default: 1
+    })
+    exhauster_number: number;
+
+    // НОМЕР ПОДШИПНИКА
+    @Column({
+        nullable: true
+    })
+    pod_number: number;
+
+    @Column({
+        nullable: false,
+        default: false
+    })
+    is_temperature: boolean;
+
+    @Column({
+        nullable: false,
+        default: false
+    })
+    is_vibration_axial: boolean;
+
+    @Column({
+        nullable: false,
+        default: false
+    })
+    is_vibration_horizontal: boolean;
+
+    @Column({
+        nullable: false,
+        default: false
+    })
+    is_vibration_vertical: boolean;
+
+    @Column({
+        nullable: false,
         default: '',
     })
-    key: string;
+    mapping_key: string;
 }
