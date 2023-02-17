cada7801d8e1c974bd0744fd99e522e1badbbb66
Artem A <akutin_artem@mail.ru>
2023-02-18 00:54:08 +0400

    refactor (#4)

--- backend/src/consumer.ts
+++ backend/src/consumer.ts
@@ -49,55 +49,5 @@ export class Consumer implements OnModuleInit, OnApplicationShutdown {
         //     },
         // })
 
-        // РАСКОМЕНТИТЬ ДЛЯ ЗАГРУЗКИ
-
-        // const results: {
-        //     place: string,
-        //     type: string,
-        //     comment: string,
-        //     exhauster: string,
-        //     active: string
-        // }[] = [];
-        // fs.createReadStream(path.join(__dirname, '../../backend/db/sample/signals_kafka.csv'))
-        //     .pipe(csv())
-        //     .on('data', (data) => results.push(data))
-        //     .on('end', async () => {
-        //         for (const el of results) {
-        //             const { type, comment, exhauster, active } = el;
-        //             const place = el[Object.keys(el)[0]];
-        //
-        //             const entity = new MappingEntity();
-        //             entity.active = Number(active);
-        //             entity.exhauster = Number(exhauster);
-        //             entity.comment = comment;
-        //             entity.type = type;
-        //             entity.place = place;
-        //
-        //             await this.repository.insert(entity)
-        //         }
-        //     });
-
-
-// Reading our test file
-//         const file = reader.readFile(path.join(__dirname, '../../backend/db/sample/signals.xlsx'))
-//         const PLACE_EXCEL_KEY = 'Код сигнала в Kafka';
-//         const KEY_KEY = '__EMPTY_2';
-//         let data = []
-//
-//         const sheets = file.SheetNames
-//
-//         for (let i = 0; i < 6; i++) {
-//             const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]])
-//
-//             for (const res of temp) {
-//                 const e = new ExcelMappingEntity();
-//                 e.place = res[PLACE_EXCEL_KEY];
-//                 e.key =  res[KEY_KEY];
-//
-//                 await this.repositoryExcel.insert(e);
-//             }
-//         }
-//
-
     }
 }
