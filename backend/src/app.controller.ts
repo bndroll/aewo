c450e970298aa35e57bf3d16e47405170312d24e
bounderoll <bounderoll.23@gmail.com>
2023-02-18 17:08:34 +0400

    update router to return 6 last documents

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {
+	constructor(
+		private readonly appService: AppService,
+	) {
+	}
+
+	@Get()
+	async getLastInfo() {
+		return await this.appService.getLastInfo();
 	}
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import DataMapper from './data-mapper';
+import { ExgMetricsService } from './exg-metrics/exg-metrics.service';
 
 
 @Injectable()
 export class AppService {
+	constructor(
+		private readonly dataMapper: DataMapper,
+		private readonly metricsService: ExgMetricsService
+	) {
+	}
 
+	async getLastInfo() {
+		return await this.metricsService.findMetric(6);
+	}
 }
