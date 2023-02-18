e83c4be819690fd6dd0d96aae03795c648fb81ca
NesQ1kx <111gosha11@gmail.com>
2023-02-18 13:36:44 +0400

    main screen components

--- backend/src/socket/socket.gateway.ts
+++ backend/src/socket/socket.gateway.ts
@@ -1,20 +1,28 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
-import { SocketService } from './socket.service';
-import { from, map } from 'rxjs';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from "@nestjs/websockets";
+import { SocketService } from "./socket.service";
+import { from, map } from "rxjs";
 
-
-@WebSocketGateway(80, {transports: ['websocket']})
+@WebSocketGateway(9000, {
+  cors: {
+    origin: "*",
+  },
+})
 export class SocketGateway {
-	constructor(private readonly socketService: SocketService) {
-	}
+  constructor(private readonly socketService: SocketService) {}
 
-	@SubscribeMessage('get_metrics')
-	findAll() {
-		return from([1, 2, 3]).pipe(map(item => ({event: 'get_metrics', data: item})));
-	}
+  @SubscribeMessage("get_metrics")
+  findAll() {
+    return from([1, 2, 3]).pipe(
+      map((item) => ({ event: "get_metrics", data: item }))
+    );
+  }
 
-	@SubscribeMessage('findOneSocket')
-	findOne(@MessageBody() id: number) {
-		return this.socketService.findOne(id);
-	}
+  @SubscribeMessage("findOneSocket")
+  findOne(@MessageBody() id: number) {
+    return this.socketService.findOne(id);
+  }
 }
--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="nav-bar py-1 px-2 d-flex align-center elevation-1">
-    <img src="../assets/logo.png" alt="" />
+    <img src="../assets/logo.png" alt="" class="mr-8" />
+    <div class="breadcrumbs d-flex text-body-2">
+      <div
+        @click="$router.push({ name: 'home' })"
+        :class="{
+          'active-route': $route.name === 'home',
+          'default-route': $route.name !== 'home',
+        }"
+      >
+        Прогнозная аналитика эксгаустеров
+      </div>
+      <div
+        class="mx-1 default-route"
+        v-if="$route.name === 'exhauster' || $route.name === 'dashboard'"
+      ></div>
+      <div
+        v-if="$route.name === 'exhauster' || $route.name === 'dashboard'"
+        :class="{
+          'active-route':
+            $route.name === 'exhauster' || $route.name === 'dashboard',
+        }"
+      >
+        / Состояние эксгаустера X-172
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,5 +39,18 @@ export default {
   height: 50px;
   width: 100%;
   background-color: white;
+}
+
+.active-route {
+  font-weight: 500;
+  color: #f57f29;
+}
+
+.default-route {
+  color: #9e9e9e;
+}
+
+.breadcrumbs {
+  cursor: pointer;
 }
 </style>
