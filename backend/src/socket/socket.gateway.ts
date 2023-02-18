95c16c8d8c30cf00a0324f600e35d43389bd00ed
bounderoll <bounderoll.23@gmail.com>
2023-02-18 21:04:11 +0400

    update socket service

--- backend/src/socket/socket.gateway.ts
+++ backend/src/socket/socket.gateway.ts
@@ -1,27 +1,24 @@
-import {
-	ConnectedSocket,
-	MessageBody,
-	SubscribeMessage,
-	WebSocketGateway,
-} from "@nestjs/websockets";
-import { SocketService } from "./socket.service";
-import { from, map } from "rxjs";
-import {Socket} from "socket.io";
+import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { SocketService } from './socket.service';
+import { from, map } from 'rxjs';
+import { Socket } from 'socket.io';
+
 
 @WebSocketGateway(9000, {
-  cors: {
-    origin: "*",
-  },
+	cors: {
+		origin: '*'
+	}
 })
 export class SocketGateway {
-  constructor(private readonly socketService: SocketService) {}
+	constructor(private readonly socketService: SocketService) {
+	}
 
-  @SubscribeMessage("get_metrics")
-  findAll() {
-    return from([1, 2, 3]).pipe(
-      map((item) => ({ event: "get_metrics", data: item }))
-    );
-  }
+	@SubscribeMessage('get_metrics')
+	findAll() {
+		return from([1, 2, 3]).pipe(
+			map((item) => ({event: 'get_metrics', data: item}))
+		);
+	}
 
 	@SubscribeMessage('exhauster')
 	getDataByExhauster(@MessageBody() data: number,
@@ -32,10 +29,5 @@ export class SocketGateway {
 	@SubscribeMessage('exhausterAll')
 	getAllData(@ConnectedSocket() client: Socket) {
 		this.socketService.addMainConnection(client);
-	}
-
-	@SubscribeMessage('findOneSocket')
-	findOne(@MessageBody() id: number) {
-		return this.socketService.findOne(id);
 	}
 }
--- backend/src/socket/socket.service.ts
+++ backend/src/socket/socket.service.ts
@@ -1,36 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
-import {Socket} from "socket.io";
-import {EventObject} from "../types";
+import { Socket } from 'socket.io';
+import { EventObject } from '../types';
 
 
 @Injectable()
 export class SocketService {
 	private mainConnection: Socket | null = null;
-	private readonly connections: Map<number, Socket> = new Map()
-
-	constructor() {
-	}
-	create(createSocketDto: CreateSocketDto) {
-		return 'This action adds a new socket';
-	}
-
-	findAll() {
-		return `This action returns all socket`;
-	}
-
-	findOne(id: number) {
-		return `This action returns a #${id} socket`;
-	}
-
-	update(id: number, updateSocketDto: UpdateSocketDto) {
-		return `This action updates a #${id} socket`;
-	}
-
-	remove(id: number) {
-		return `This action removes a #${id} socket`;
-	}
+	private readonly connections: Map<number, Socket> = new Map();
 
 	addConnection(exhNumber: number, connection: Socket) {
 		this.connections.set(exhNumber, connection);
@@ -48,7 +24,7 @@ export class SocketService {
 		this.mainConnection = null;
 	}
 
-	emitData(data: Record<number,EventObject>) {
+	emitData(data: Record<number, EventObject>) {
 		if (this.mainConnection) {
 			this.mainConnection.emit('data', data);
 		}
